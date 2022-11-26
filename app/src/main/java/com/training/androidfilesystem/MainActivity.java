6e0498657424165619ab2930e783f7ba7ede9767
yokesh7373@gmail.com <gitHub@1>
2022-11-26 22:21:47 +0530

    Initial project setup

--- app/src/main/java/com/training/androidfilesystem/MainActivity.java
+++ app/src/main/java/com/training/androidfilesystem/MainActivity.java
@@ -3,12 +3,37 @@ package com.training.androidfilesystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnInternalStorage;
+    Button btnCacheStorage;
+    Button btnExternalStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnInternalStorage = findViewById(R.id.btnInternalStorage);
+        btnCacheStorage = findViewById(R.id.btnCacheStorage);
+        btnExternalStorage = findViewById(R.id.btnExternalStorage);
+
+        btnInternalStorage.setOnClickListener(v -> launchInternalStorageActivity());
+        btnCacheStorage.setOnClickListener(v -> launchCacheStorageActivity());
+        btnExternalStorage.setOnClickListener(v -> launchExternalStorageActivity());
+    }
+
+    private void launchInternalStorageActivity() {
+
+    }
+
+    private void launchCacheStorageActivity() {
+
+    }
+
+    private void launchExternalStorageActivity() {
+
     }
 }
